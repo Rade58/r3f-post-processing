fd061abfcac8895d4b023e9234f5a801fd1bdcb6
rade <bajic.rade2@gmail.com>
2025-01-20 15:12:37 +0100

    initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# R3F Workshops - Post Processing
+
+- Bootstraped with:
+
+```
+pnpm create vite
+```
+
+- dependancies
+
+```
+pnpm add three @react-three/fiber @react-three/drei leva@0.9.34
+```
+
+```
+pnpm add -D r3f-perf @types/three
+```
+
+# Leva @0.9.34
+
+---
+
+---
+
+# About
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+// -----------------------------------------------------------
+// import { App } from "./0_setup/App";  // with Stage, no light, no shadows enabled
+import { App } from "./1_setup/App"; // with lights without Stage
+// -----------------------------------------------------------
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
