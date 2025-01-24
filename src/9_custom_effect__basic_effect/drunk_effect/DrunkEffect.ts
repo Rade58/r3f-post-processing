db76245956418ed0fb3f5184bf67e2e92cbe77ab
rade <bajic.rade2@gmail.com>
2025-01-24 14:37:47 +0100

    🚀

--- src/9_custom_effect__basic_effect/drunk_effect/DrunkEffect.ts
+++ src/9_custom_effect__basic_effect/drunk_effect/DrunkEffect.ts
@@ -3,7 +3,7 @@ import { Effect } from "postprocessing";
 import fragmentShader from "./fragment.glsl";
 
 export class DrunkEffect extends Effect {
-  constructor(name: string) {
-    super(name, fragmentShader);
+  constructor() {
+    super("DrunkEffect", fragmentShader, {});
   }
 }
--- src/9_custom_effect__basic_effect/drunk_effect/fragment.glsl
+++ src/9_custom_effect__basic_effect/drunk_effect/fragment.glsl
@@ -11,10 +11,23 @@
 //                calling the mainImage function
 // out :     changing this value will change the variable sent
 //             when calling the mainImage function
+
+
+// It prevents us from making mistakes but also gives us a
+// hint about what variables we need to change
+// - inputColor contains the current color for that pixel
+// which is defined by the previous effects
+// - uv contains the render coordinates
+// - outputColor is what we need to change in order to apply
+//  the effect
+
+
 void mainImage(
   const in vec4 inputColor,
   const in vec2 uv,
   out vec4 outputColor
 ){
-  // 
+  
+  outputColor = vec4(uv, 1.0, 1.0);
+
 }
